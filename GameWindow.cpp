88c03abee87024e624a4a5d5462c1f139e3831b1
zvei <zhongvei@gmail.com>
2021-11-17 00:46:37 +0800

    added health bar

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -4,17 +4,36 @@
 #include "GameEntity/View.h"
 #include "GameEntity/Block.h"
 #include <cstdlib>
+#include <QIcon>
+#include <QPointF>
 
+Basic* health_bar = new Basic();
 GameWindow::GameWindow(QWidget* parent)
 {
+    this->setWindowTitle("TankOOP");
+    this->setWindowIcon(QIcon(R"(C:\Users\zhong\Desktop\Uni\Academic Semester\Fall 2021\COMP 2012H\Tankoop\resource\gameIcon.jpeg)"));
     scene = new QGraphicsScene();
 
     setScene(scene);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+    // set size of view (game window) and scene (entire map) as maximum to draw background
+    setFixedSize(2000,2000);
+    setSceneRect(0,0,2000,2000);
+
+    // set background color and draw background grid
+    this->setStyleSheet("background:rgb(204,204,204)");
+    QPen lineColor(QColor(196, 196, 196));
+    for(int x = -1; x <= this->width(); x += 28){
+        scene->addLine(x,0,x,this->height(),lineColor);
+    }
+    for(int y = -2; y <= this->height(); y += 28){
+        scene->addLine(0,y,this->width(),y,lineColor);
+    }
+    // reduce size of view (game window) to appropriate size
     setFixedSize(1200,600);
-    setSceneRect(0,0,1000000,1000000);
+
     spawn_loop();
 //    MyRect* enemy = new MyRect();
 //    enemy->setRect(0,0,100,100);
@@ -36,6 +55,10 @@ GameWindow::GameWindow(QWidget* parent)
     loop_timer->start();
 
 
+    health_bar->setRect(0,0,100,20);
+    health_bar->setPos(100,200);
+    scene->addItem(health_bar);
+
     show();
 
 }
@@ -44,6 +67,14 @@ void GameWindow::main_loop() {
 //    float x = rect->x();
 //    float y = rect->y();
     centerOn(basic);
+
+    QPointF tankpos;
+    tankpos.setX(basic->x());
+    tankpos.setY(basic->y());
+    tankpos += QPointF(0,120);
+    health_bar->setPos(tankpos);
+//    QPointF pos = health_bar->mapToItem(basic, 0, 100);
+//    health_bar->setPos(pos);
 }
 
 void GameWindow::spawn_loop() {
